7adb2f3b7715fe007022d4f9305c7427981fa50f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-14 16:33:36 -0300

    verificações dos valores digitados

--- javaScript/main.js
+++ javaScript/main.js
@@ -3,12 +3,18 @@ let respostaSequenciaFibonacci = document.getElementById("resposta");
 
 function calculaSequenciaFibonacci() {
     let numero = campoNumero.value;
-    
-    let fibonacci = numero;
-    for (let i = numero - 1; i >= 1; i--) {
-        fibonacci = fibonacci * i;
+
+    if (isNaN(numero)) {
+        respostaSequenciaFibonacci.textContent = "Digite somente números!";
+    } else if (numero < 0) {
+        respostaSequenciaFibonacci.textContent = "Digite somente números positivos!";
+    } else {
+        let fibonacci = numero;
+        for (let i = numero - 1; i >= 1; i--) {
+            fibonacci = fibonacci * i;
+        }
+        exibeRespostaSequenciaFibonacci(numero, fibonacci);
     }
-    exibeRespostaSequenciaFibonacci(numero, fibonacci);
 }
 
 function exibeRespostaSequenciaFibonacci(numero, fibonacci) {
@@ -17,7 +23,11 @@ function exibeRespostaSequenciaFibonacci(numero, fibonacci) {
 
 function apagaRespostaSequenciaFibonacci() {
     respostaSequenciaFibonacci.textContent = "";
+    campoNumero.value = "";
 }
 
 let botaoCalcular = document.getElementById("botaoCalcular");
 botaoCalcular.onclick = calculaSequenciaFibonacci;
+
+let botaoApagar = document.getElementById("botaoApagar");
+botaoApagar.onclick = apagaRespostaSequenciaFibonacci;

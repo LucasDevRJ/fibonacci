d31d0fe02878c2581aa0a53e8aa53525cbbb27f8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-14 00:12:22 -0300

    criando elementos html no js

--- javaScript/main.js
+++ javaScript/main.js
@@ -5,6 +5,17 @@ function calculaSequenciaFibonacci() {
     for (let i = numero - 1; i >= 1; i--) {
         numero = numero * i;
     }
+    exibeRespostaSequenciaFibonacci(numero);
+}
+
+function exibeRespostaSequenciaFibonacci(numero) {
+    let secao = document.querySelector("section");
+
+    let p = document.createElement("p");
+    let fibonacci = document.createTextNode(numero);
+    p.appendChild(fibonacci);
+
+    secao.appendChild(p);
 }
 
 let botao = document.getElementById("botao");

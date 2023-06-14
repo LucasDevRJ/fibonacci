2dfdc32627a4b953d75b9da0ea4311ace89bff91
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-13 23:49:50 -0300

    lógica da sequencia de fibonacci concluída

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,2 +1,11 @@
-let campoNumero = document.getElementById("numero");
-let numero = campoNumero.value;
+function calculaSequenciaFibonacci() {
+    let campoNumero = document.getElementById("numero");
+    let numero = campoNumero.value;
+    
+    for (let i = numero - 1; i >= 1; i--) {
+        numero = numero * i;
+    }
+}
+
+let botao = document.getElementById("botao");
+botao.onclick = calculaSequenciaFibonacci;
